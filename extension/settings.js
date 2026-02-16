84153f3558650a2a05f9e8297021ab39625a940a
Jesse <jesse@fictiontoreality.dev>
2026-02-16 00:18:04 -0800

    feat: Add multi-tab support and right-click context menus (#74)
    Allows selecting multiple tabs and exporting them to:
    - clipboard (concatenated)
    - single Markdown file (concatenated)
    - ZIP of individual Markdown files
    
    To accomplish this, a new popup menu for multi-tab is added.
    
    Also hotkeys for the clipboard and single Markdown file actions are shared with their single-tab counterparts, while ZIP introduces a new Alt+Shift+Z shortcut. This minimizes the amount of hotkeys needed to use the add-on.
    
    Tested on Chrome and Firefox.
    
    -----
    
    Right-click context menus
    
    For those who don't use the addon often enough to memorize hotkeys, having a
    context menu is very convenient.
    
    Tested on Chrome and Firefox. On Firefox tab context menus are also
    available (unfortunately Chrome doesn't seem to support this).
    
    Additional changes:
    
    * Add concurrency to multi-tab processing
    * Await loading of multi-tab data URLs.
    * Create user settings utility to consolidate settings access.
    * Warn when processing large numbers of tabs.

---
+++ extension/settings.js
@@ -0,0 +1,31 @@
+// LLMFeeder Settings Utilities
+// Shared settings constants and functions used across popup and background scripts
+
+const SettingsUtils = (function() {
+  // Default metadata format template
+  const DEFAULT_METADATA_FORMAT = "---\nSource: [{title}]({url})";
+
+  // Get user settings with defaults
+  async function getUserSettings(browserAPI) {
+    return await browserAPI.storage.sync.get({
+      contentScope: 'mainContent',
+      preserveTables: true,
+      includeImages: true,
+      includeTitle: true,
+      includeMetadata: true,
+      metadataFormat: DEFAULT_METADATA_FORMAT,
+      debugMode: false
+    });
+  }
+
+  // Public API
+  return {
+    DEFAULT_METADATA_FORMAT,
+    getUserSettings
+  };
+})();
+
+// For use in browser extension contexts (not modules)
+if (typeof window !== 'undefined') {
+  window.SettingsUtils = SettingsUtils;
+}
